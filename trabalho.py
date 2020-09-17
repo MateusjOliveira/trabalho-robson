10dcebc84d7fa921f41089e6019c1fa14686e902
MateusjOliveira <63262677+MateusjOliveira@users.noreply.github.com>
2020-09-17 14:24:10 -0300

    Update trabalho.py

--- trabalho.py
+++ trabalho.py
@@ -16,12 +16,13 @@ get_ipython().system('pip install flask-ngrok')
 get_ipython().system('pip install flask-cors')
 
 
-# In[7]:
+# In[1]:
 
 
 from flask_ngrok import run_with_ngrok
 from flask import Flask, request, jsonify
 from flask_cors import CORS
+import os
 
 
 # In[ ]:
@@ -30,7 +31,7 @@ from flask_cors import CORS
 app = Flask(__name__)
 
 CORS(app)
-run_with_ngrok(app)
+#run_with_ngrok(app)
 
 @app.route('/')
 def root():
@@ -67,7 +68,9 @@ def sum(v1,v2):
     response = jsonify(soma)
     return response
 
-app.run()
+if __name__ == "__main__":
+    port = int(os.environ.get("PORT", 5000))
+    app.run(host="0.0.0.0", port=port)
 
 
 # In[ ]:
